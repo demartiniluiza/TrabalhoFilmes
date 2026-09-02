48b19921050f4a0d2c8d0fa28a58531cbfaf23de
demartiniluiza <demartinidacostaluiza@gmail.com>
2026-09-02 11:04:31 -0300

    Capas reais dos filmes no seeder

--- database/seeders/FilmeSeeder.php
+++ database/seeders/FilmeSeeder.php
@@ -21,7 +21,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'Um hacker descobre que a realidade é uma simulação e entra na luta contra as máquinas.',
                 'ano' => 1999,
                 'categoria' => 'Ficção Científica',
-                'capa' => 'https://placehold.co/300x450/1f1f2e/ffffff?text=Matrix',
+                'capa' => 'https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=vKQi3bBA1y8',
             ],
             [
@@ -29,7 +29,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'Exploradores viajam por um buraco de minhoca em busca de um novo lar para a humanidade.',
                 'ano' => 2014,
                 'categoria' => 'Ficção Científica',
-                'capa' => 'https://placehold.co/300x450/12263a/ffffff?text=Interestelar',
+                'capa' => 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=zSWdZVtXT7E',
             ],
             [
@@ -37,7 +37,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'A saga de uma família da máfia italiana e a ascensão de seu filho mais novo ao poder.',
                 'ano' => 1972,
                 'categoria' => 'Drama',
-                'capa' => 'https://placehold.co/300x450/2b2b2b/ffffff?text=O+Poderoso+Chefao',
+                'capa' => 'https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=sY1S34973zA',
             ],
             [
@@ -45,7 +45,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'A origem de um comediante fracassado que se torna um dos maiores vilões de Gotham.',
                 'ano' => 2019,
                 'categoria' => 'Drama',
-                'capa' => 'https://placehold.co/300x450/3a2e12/ffffff?text=Coringa',
+                'capa' => 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=zAGVQLHvwOY',
             ],
             [
@@ -53,7 +53,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'Os brinquedos de um menino ganham vida quando ninguém está olhando.',
                 'ano' => 1995,
                 'categoria' => 'Animação',
-                'capa' => 'https://placehold.co/300x450/123a2e/ffffff?text=Toy+Story',
+                'capa' => 'https://image.tmdb.org/t/p/w500/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=wmiIUN-7qhE',
             ],
             [
@@ -61,7 +61,7 @@ class FilmeSeeder extends Seeder
                 'sinopse' => 'Um rapaz visita a família da namorada e descobre segredos assustadores.',
                 'ano' => 2017,
                 'categoria' => 'Terror',
-                'capa' => 'https://placehold.co/300x450/3a1212/ffffff?text=Corra',
+                'capa' => 'https://image.tmdb.org/t/p/w500/tFXcEccSQMf3lfhfXKSU9iRBpa3.jpg',
                 'trailer' => 'https://www.youtube.com/watch?v=DzfpyUB60YY',
             ],
         ];
